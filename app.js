45f57a16666480432426c2bb3a6568db693926cf
DonPiantissimo <DonPiantissimo@hotmail.com>
2017-06-23 02:55:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ var
         express = require('express'),
         http = require('http'),
         app = express(),
-        server = http.createServer(app);
+        server = http.createServer(app),
+	bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false })) 
+
+app.use(bodyParser.json());
+
+
+
+
 
 server.listen(gameport);
 
@@ -24,29 +33,36 @@ app.get('/*', function (req, res, next) {
     res.sendFile(__dirname + '/' + req.params[0]);
 });
 
+
+
 var sio = io.listen(server);
+
 
 
 app_server = require('./server.js');
 var data = {lv:1, safety:false};
 var mes="";
 
+app.post('/', function(req, res) {
+	console.log('spin');
+		data.lv = parseInt(req.body.level);
+                if (req.body.HumbleSpin == "on") data.safety = true;
+		else data.safety = false;
+                if (data.lv>0 && data.lv<20) {
+                    mes=app_server.onMessage(data);
+                    console.log('emitting '+mes);
+                    
+                }
+		else 
+                    mes = "error";
+		sio.emit('result', mes);
+});
 
 sio.sockets.on('connection', function (client) {
     client.emit('onconnected', {});
     console.log('connected');
     
-    	client.on('spin', function(msg){
-            console.log('spin');
-		data.lv = parseInt(msg);
-                if (data.lv>0 && data.lv<20) {
-                    mes=app_server.onMessage(data);
-                    console.log('emitting '+mes);
-                }
-		else 
-                    mes = "error";
-		sio.emit('result', mes);
-	});
+
     
     client.on('disconnect', function () {
 
